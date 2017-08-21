02eba39882b925bd776ef3b8524483cc80128780
zhangwl <zhangweilai@126.com>
2017-08-21 12:56:41 +0800

    init project

---
+++ src/main/java/cn/zwl/springboot/configclient/ConfigClientApplication.java
@@ -0,0 +1,23 @@
+package cn.zwl.springboot.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+	@Value("${bar}")
+	private String configValue;
+	
+	@RequestMapping("/")
+    public String home() {
+        return "Hello World!--"+configValue;
+    }
+	
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientApplication.class, args);
+  }
+}
